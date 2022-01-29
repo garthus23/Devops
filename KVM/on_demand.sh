1a96e3cddfaf33559df989c5bdf4825f1bdadd54
Greg <gr.arthus@gmail.com>
2022-01-29 23:45:07 +0000

    remove duplicate if commands

--- KVM/on_demand.sh
+++ KVM/on_demand.sh
@@ -6,16 +6,15 @@
 
 if [[ $1 && $2 ]]
 then
+	sed -i s/"IMAGE_NAME.*"/"IMAGE_NAME=$2"/g vars.sh
+	echo "local-hostname: $2" > meta-data
+	
 	if [[ $1 == 'centos' ]]
 	then
-		sed -i s/"IMAGE_NAME.*"/"IMAGE_NAME=$2"/g vars.sh
-		echo "local-hostname: $2" > meta-data
 		./cloudinit_centos.sh
 	fi
 	if [[ $1 == 'ubuntu' ]]
 	then
-		sed -i s/"IMAGE_NAME.*"/"IMAGE_NAME=$2"/g vars.sh
-		echo "local-hostname: $2" > meta-data
 		./cloudinit_ubuntu.sh
 	fi
 else
