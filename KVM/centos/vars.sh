91149fa76dfdd6900c228bf491c967049e636b57
Greg ! <gr.arthus@gmail.com>
2022-07-03 13:41:21 +0200

    ansible user-date creation

--- KVM/centos/vars.sh
+++ KVM/centos/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=pioupiou
+IMAGE_NAME=testenv
 IMAGE_FOLDER='/home/greg/isos'
 DISK_FOLDER='/home/greg/disks'
 NET='isolated'
--- KVM/ubuntu/vars.sh
+++ KVM/ubuntu/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=toutou
+IMAGE_NAME=testenv1
 IMAGE_FOLDER='/home/greg/isos'
 DISK_FOLDER='/home/greg/disks'
 NET='isolated'
