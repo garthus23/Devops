3bf5016c9134f6119f49623229a08c07b0e5f26c
greg <gr.arthus@gmail.com>
2023-01-15 18:12:48 +0000

    get domain ip address

--- KVM/rocky/vars.sh
+++ KVM/rocky/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=toto
+IMAGE_NAME=worker2
 IMAGE_FOLDER='/opt/data/isos'
 DISK_FOLDER='/opt/data/disks'
 NET='default'
--- KVM/ubuntu/vars.sh
+++ KVM/ubuntu/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=totu
+IMAGE_NAME=worker1
 IMAGE_FOLDER='/opt/data/isos'
 DISK_FOLDER='/opt/data/disks'
 NET='default'
