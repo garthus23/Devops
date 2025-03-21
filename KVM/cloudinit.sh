ec764dad69434ad6ad41114bdcd9e0e6bd55145c
Greg <garthus@adminux.fr>
2025-03-21 09:21:57 +0100

    merge os folders, add uefi

--- KVM/cloudinit.sh
+++ KVM/cloudinit.sh
@@ -2,13 +2,20 @@
 # cloud image deploy Rocky Linux 9 on Qemu/KVM
 
 # set var file 
-if [[ -f ../vars.sh ]]
+if [[ -f ./vars.sh ]]
 then
-	. ../vars.sh
+	. ./vars.sh
 fi
 
-DLNAME='https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2'
-
+if [[ $1 == 'rocky' ]]
+then
+	DLNAME='https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2'
+elif [[ $1 == 'ubuntu' ]]
+then
+	DLNAME='https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img'
+else
+	echo "OS $1 not found"
+fi
 # retrieve cloud image from ubuntu website
 if [[ ! -f "$IMAGE_FOLDER/$(basename $DLNAME)" ]]
 then
@@ -34,7 +41,7 @@ xorriso -as mkisofs  -output $IMAGE_FOLDER/cidata.iso -V cidata -r -J user-data 
 
 # launch install 
 #--graphics spice,listen=127.0.0.1 
-virt-install --connect qemu:///system --virt-type kvm --name $IMAGE_NAME --ram 2048 --vcpus=2 --os-variant rhl9 --import --disk path=$DISK_FOLDER/$IMAGE_NAME.qcow2,format=qcow2 --disk path=$IMAGE_FOLDER/cidata.iso,device=cdrom --network network=$NET --noautoconsole --graphics none
+virt-install --connect qemu:///system --virt-type kvm --name $IMAGE_NAME --ram 2048 --vcpus=2 --os-variant linux2022 --import --disk path=$DISK_FOLDER/$IMAGE_NAME.qcow2,format=qcow2 --disk path=$IMAGE_FOLDER/cidata.iso,device=cdrom --network network=$NET --noautoconsole --graphics none --boot uefi
 
 # detach cdrom on next boot
 virsh change-media $IMAGE_NAME $IMAGE_FOLDER/cidata.iso --config --eject
--- KVM/vars.sh
+++ KVM/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=test
+IMAGE_NAME=toto
 IMAGE_FOLDER='/var/lib/libvirt/isos'
 DISK_FOLDER='/var/lib/libvirt/images'
 NET='default'
